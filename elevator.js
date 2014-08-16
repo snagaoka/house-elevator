d14dbe6cff3201c68dc73a35975a300847ccd407
snagaoka <shannonnagaoka@gmail.com>
2014-08-16 11:36:10 -1000

    set 'fifth floor to first floor' to not move, and set 'not take objects' now passing

--- elevator.js
+++ elevator.js
@@ -4,19 +4,25 @@
 module.exports = {
 	goto: function (currentFloor, nextFloor){
 
+		if (currentFloor < 4) {
 
-		if (parseInt(nextFloor) < 4) {
+			if (parseInt(nextFloor) < 4) {
 
-		var newDestination = parseInt(nextFloor) - currentFloor;
- 
-		return parseInt(newDestination);
+			var newDestination = parseInt(nextFloor) - currentFloor;
+	 
+			return parseInt(newDestination);
+
+			} else {
+
+				return 0
+
+			}
 
 		} else {
 
 			return 0
 
 		}
-		
 
 	}
 
